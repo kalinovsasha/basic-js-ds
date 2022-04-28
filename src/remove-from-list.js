62db45708448292fbe91084cbc2712c2d084e256
Sasha_work <kalinovsasha@yandex.ru>
2022-04-28 10:54:37 +0300

    fix: should return the list without values equal to k (with double k)

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,26 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
- const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
-/**
- * Given a singly linked list of integers l and an integer k,
- * remove all elements from list l that have a value equal to k.
- *
- * @param {List} l
- * @param {Number} k
- * @return {List}
- *
- * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
- * the output should be [1, 2, 4, 5]
- *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
  function convertArrayToList(arr) {
   return arr.reverse().reduce((acc, cur) => {
     if (acc) {
@@ -32,10 +12,7 @@ const { NotImplementedError } = require('../extensions/index.js');
     return new ListNode(cur);
   }, null);
 }
-let initial = convertArrayToList([3, 1, 2, 3, 4, 5]);
-initial = convertArrayToList([1, 2, 3, 3, 4, 5]);
-const expected = convertArrayToList([1, 2, 4, 5]);
-///////////////////////////////////////////////////////////
+////////////////////////////Solution//////////////////////////////
 function removeKFromList(l, k ) {
   let current=l;
   let prev;
@@ -46,19 +23,21 @@ function removeKFromList(l, k ) {
       } else{
         l=current.next
       }
+      current=l;
     }
     prev=current  
     current=current.next
-  } 
-    
+  }  
   return l
 }
-
+/* /Test/
+let initial = convertArrayToList([1, 2, 3, 3, 4, 5]);
+const expected = convertArrayToList([1, 2, 4, 5]);
 console.log(removeKFromList(initial, 3))
 console.log(removeKFromList(initial, 3).next)
 console.log(removeKFromList(initial, 3).next.next)
 console.log(removeKFromList(initial, 3).next.next.next)
-
+*/
 module.exports = {
   removeKFromList
 };
