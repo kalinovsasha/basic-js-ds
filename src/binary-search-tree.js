09ea37fd5c0b0fdaa8770c6de229509085318d06
Alexander <kalinovsasha@yandex.ru>
2022-04-30 13:56:07 +0300

    feat:add methods root,add to BinarySearchTree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,59 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const { Node } = require('../extensions/list-tree.js');
 
-// const { Node } = require('../extensions/list-tree.js');
-
-/**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
 class BinarySearchTree {
-
+    constructor(){
+      this.treeRoot=null;
+    }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+     this.treeRoot= addNode(this.treeRoot,data)
+     function addNode(node,data){
+       if(!node){
+         return new Node(data)
+       }
+       if(node.data==data){
+         return node
+       }
+       if(data<node.data){
+         node.left=addNode(node.left,data)
+       }else{
+        node.right=addNode(node.right,data)
+       }
+       return node
+     }
+
   }
 
   has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 
   find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 
   remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 }
 
 module.exports = {
   BinarySearchTree
 };
+
+tree = new BinarySearchTree;
+tree.add(1);
+tree.root()
+console.log(tree.root())
