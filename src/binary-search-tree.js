6ad026af587688f0c524ed00b4be7c99e0ac011c
Alexander <kalinovsasha@yandex.ru>
2022-04-30 14:27:40 +0300

    feat:add methods find,has to BinarySearchTree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -28,11 +28,44 @@ class BinarySearchTree {
 
   }
 
-  has(/* data */) {
+  has(data) {
+    return searchNode(this.treeRoot,data)
+
+    function searchNode(node,data){
+      if(!node){
+        return false
+      }
+      if(node.data==data){
+        return true
+      }
+      if(data<node.data){
+        return searchNode(node.left,data)
+      }
+      if(data>node.data){
+        return searchNode(node.right,data)
+      }
+      
+    }
     
   }
 
-  find(/* data */) {
+  find(data) {
+    return findNode(this.treeRoot,data)
+
+    function findNode(node,data){
+      if(!node){
+        return node
+      }
+      if(node.data==data){
+        return node
+      }
+      if(data<node.data){
+        return findNode(node.left,data)
+      }
+      if(data>node.data){
+        return findNode(node.right,data)
+      }
+    }
     
   }
 
@@ -54,6 +87,11 @@ module.exports = {
 };
 
 tree = new BinarySearchTree;
-tree.add(1);
-tree.root()
+tree.add(30);
+tree.add(20);
+tree.add(40);
+tree.add(50);
+tree.root(35)
 console.log(tree.root())
+console.log(tree.has(30))
+console.log(tree.find(40))
